6a59c71265d6b06764929c30c5d72f14e8f5f1a2
xiaomingzhong <845393842@qq.com>
2019-06-15 11:01:39 +0800

    Update resign.sh

--- resign.sh
+++ resign.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
-location=/home/xiaoming/Desktop/resign/tools
-project=${location}/HelloWorld
 
-keystore=${location}/test.jks
+
+location=/home/xiaoming/Downloads/xiaoming/resign
+apk_tmp=tmp.apk
+apk_input=source.apk
+apk_out=out-resigner.apk
+
+zipalign=/home/xiaoming/Android/Sdk/build-tools/23.0.3/zipalign
+
+keystore=/home/xiaoming/Downloads/xiaoming/resign/test.jks
 alias=test
-storepass=125456
-keypass=125489
+storepass=654321
+keypass=654321
 
-cd ${project}
-rm -r -f ${project}/META-INF/
-zip -r ${project}/product.apk *
-mv ${project}/product.apk ..
-cd ..
-jarsigner -keystore ${keystore} -storepass ${storepass} -keypass ${keypass} ${location}/product.apk  ${alias} 
-/home/xiaoming/Android/Sdk/build-tools/26.0.2/zipalign 4 ${location}/product.apk ${location}/out-apk-resigner.apk
+function rezip_apk(){
+	unzip ${location}/${apk_input} -d ./tmp
+	cd tmp
+	rm -rf META-INF/
+	zip -r ${apk_tmp} *
+	mv ${apk_tmp} ../
+	cd ..
+	rm -rf ./tmp
+}
+
+function resign(){
+	jarsigner -keystore ${keystore} -storepass ${storepass} -keypass ${keypass} ${apk_tmp} ${alias} 
+
+	${zipalign} 4 ${apk_tmp} ${apk_out}
+	rm -rf ${apk_tmp}
+}
+rezip_apk
+resign
+
